1123347c99b4554aaeba6f687aef652f1ef36ab9
Pextra Pexim <pextra@Pextras-MacBook-Pro.local>
2019-05-22 11:51:49 +0200

    Fixed basic Search test.

--- src/Search.test.js
+++ src/Search.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Search from './Search';
+import Search from './components/Search';
 
 it('renders without crashing of Search', () => {
   const div = document.createElement('div');
